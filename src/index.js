ff0ab1daa39e32801fc48dee7bf1c6276e061776
Dmitriy Efimov <dima.efimo2014@yandex.ru>
2024-03-22 22:16:04 +0300

    Add some useful files and libraries

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './styles/reseter.css';
+import './styles/global.css';
+import App from './containers/App/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
